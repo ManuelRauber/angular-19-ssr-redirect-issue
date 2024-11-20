c829348f76dbc388928ab9a7a08e59bcdcb6ff0f
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2024-11-20 09:35:32 +0100

    prepare example

--- v18ssr/src/app/app.routes.ts
+++ v18ssr/src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {ExampleComponent} from './example.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'some',
+  },
+  {
+    path: ':param',
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'thing',
+      },
+      {
+        path: 'thing',
+        component: ExampleComponent,
+      }
+    ]
+  }
+];
---
+++ v19/src/app/example.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-example',
+  template: 'Hello from Example Component!'
+})
+export class ExampleComponent {
+
+}
